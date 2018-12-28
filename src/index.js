95ef1a5be908d85e44ccb0cfbee468780ce63409
lukasz_brainhub <lukasz@brainhub.pl>
2018-12-28 20:06:22 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import http from 'http';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+import checkPassword from 'middleware/checkPassword';
+import config from './config';
+import api from 'v1';
+
+import createDatabase from './services/mongoDatabaseService';
+
+(async function () {
+  if (!config.password && process.env.ENV !== 'development') {
+    throw new Error('Password not set. Set the value in ADMIN_PASSWORD env variable.');
+  }
+  const app = express();
+  app.server = http.createServer(app);
+
+  app.use(cors({
+    origin: true,
+    credentials: true,
+    exposedHeaders: config.corsHeaders,
+  }));
+
+  const db = await createDatabase();
+
+  app.use(checkPassword(config.password));
+  app.use(bodyParser.json({ limit: config.bodyLimit }), api({ db }));
+
+  // starting actual server
+  app.server.listen(config.port);
+
+  console.log(`Started on port ${app.server.address().port}`); // eslint-disable-line no-console
+
+})();
