de1699ba507f84f25d13527e882d7aee18e1dcfb
lukasz_brainhub <lukasz@brainhub.pl>
2019-01-05 13:30:43 +0100

    Updated default db name

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ### Environment variables
 - **PORT** (default: 8080)
-- **DB_NAME** (default: 'papu')
+- **DB_NAME** (default: 'chooseYourDbNameForThisBoilerplate', you can change the default in config.js)
 - **DB_HOST** (default: 'localhost:27017')
 - **ADMIN_PASSWORD** - (*required*) password to access API
 
--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {
   port: process.env.PORT || 8080,
   bodyLimit: '100mb',
   corsHeaders: ['Link', 'Jwt'],
-  dbName: process.env.DB_NAME || 'papu',
+  dbName: process.env.DB_NAME || 'chooseYourDbNameForThisBoilerplate',
   dbHost: process.env.DB_HOST || 'localhost:27017',
   password: process.env.ADMIN_PASSWORD,
 };
