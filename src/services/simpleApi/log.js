43c2bb31b4327797f8852b6ab857e608ca70bf6c
Łukasz Pluszczewski <lukasz.pluszczewski@gmail.com>
2020-03-25 17:00:57 +0100

    Removed unused code

--- src/services/simpleApi/log.js
+++
@@ -1,9 +0,0 @@
-import debug from 'debug';
-
-// prepare loggers using DEBUG utility
-const log = debug('simpleApi');
-log.request = debug('simpleApi:request');
-log.requestDetails = debug('simpleApi:requestDetails');
-log.warning = debug('simpleApi:warning');
-
-export default log;
